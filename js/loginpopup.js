ab1002a41a5a18a7810ad0eb1e36696c5e5c2e7e
Genna Weber <gennamweber@gmail.com>
2021-02-26 12:38:35 -0500

    Adds login popup

--- js/loginpopup.js
+++ js/loginpopup.js
@@ -1,7 +1,19 @@
 function openForm() {
-  document.getElementById("loginform").style.display = "block";
-}
-
-function closeForm() {
-  document.getElementById("loginform").style.display = "none";
-}
+    document.getElementById("loginForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("loginForm").style.display = "none";
+  }
+  
+  function validateForm() {
+    
+   let userName = document.forms["loginInfo"]["email"].value;
+   let pwd = document.forms["loginInfo"]["psw"].value;
+   if (userName=="xxx@email.com" && pwd=="password") {
+     alert('you did it')
+      }
+    else {
+    alert('Invalid Email or Password')
+   }
+  }
