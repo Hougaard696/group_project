514f027775a54b2c58b03922584fc105251a8f64
Christiaan Hougaard <cwhougaard@gmail.com>
2021-02-27 13:58:13 -0500

    extracted login HTML into js function

--- js/loginpopup.js
+++ js/loginpopup.js
@@ -1,5 +1,17 @@
 function openForm() {
     document.getElementById("loginForm").style.display = "block";
+    document.getElementById("loginForm").innerHTML = `<form class="formContent" name="loginInfo" id="login" >
+    <h2>Login</h2>
+    <div class="errorMessage"></div>
+    <label for="username">Username</label></b>
+    <input type="text" placeholder="Username" name="username" id="username" required>
+    <br>
+    <label for="psw">Password</label></b>
+    <input type="password" placeholder="Enter Password" name="psw" id="psw"required>
+
+    <button class="btn" value="Submit" onclick="validateForm()">Submit</button>
+    <button class="btn cancel" onclick="closeForm()">Cancel</button>
+    </form>`
   }
   
   function closeForm() {
