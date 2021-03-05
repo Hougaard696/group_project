59a389dde5e39ee26fd89b37ace1ab091dcbb39a
Genna Weber <gennamweber@gmail.com>
2021-03-05 15:01:46 -0500

    Adds more photos to the homepage slideshow('

--- js/slideshowScript.js
+++ js/slideshowScript.js
@@ -8,6 +8,18 @@ var time = 4000;
 images[0] = './images/macro.jpg';
 images[1] = './images/wildlife.jpg';
 images[2] = './images/fern.jpg';
+images[3] = './images/wildlife/ANN_6361.jpg';
+images[4] = './images/landscape/ANN_5649.jpg';
+images[5] = './images/landscape/ANN_6309.jpg';
+images[6] = './images/landscape/ANN_6323.jpg';
+images[7] = './images/landscape/ANN_6235.jpg';
+images[8] = './images/landscape/ANN_6730.jpg';
+images[9] = './images/landscape/ANN_6889.jpg';
+images[10] = './images/water/_ANN0654.jpg';
+images[11] = './images/water/ANN_5176.jpg';
+images[12] = './images/macro/_ANN1939.jpg';
+images[13] = './images/landscape/ANN_5532.jpg';
+images[14] = './images/macro/_ANN5684_2926.jpg';
 
 
 i = images.length
